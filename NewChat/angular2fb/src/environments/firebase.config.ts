341de5a594af395fa45263e4a38adeb1a08686ad
wox0 <wox0@protonmail.com>
2017-04-27 22:05:30 +0200

    New project, sync with fb

---
+++ NewChat/angular2fb/src/environments/firebase.config.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by roxwo on 27.04.2017.
+ */
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCN8mzKVqg_TmR_VZte23dC-aptbXL1KOM',
+  authDomain: 'webowe-e83e6.firebaseapp.com',
+  databaseURL: 'https://webowe-e83e6.firebaseio.com',
+  projectId: 'webowe-e83e6',
+  storageBucket: 'webowe-e83e6.appspot.com',
+  messagingSenderId: '941799242278'
+};
---
+++ NewChat/angular2fb/src/typings.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by roxwo on 27.04.2017.
+ */
+declare var require: any;
+declare var module: any;
