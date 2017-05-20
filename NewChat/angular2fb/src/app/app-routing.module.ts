f910c975dfb0ae116b9a43b9415cdcc107199a81
DariuszMak <darekmakarewicz1994@gmail.com>
2017-05-20 14:09:28 +0200

    simple guard function example

--- NewChat/angular2fb/src/app/app-routing.module.ts
+++ NewChat/angular2fb/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {ActivatedRouteSnapshot, RouterModule, RouterStateSnapshot, Routes} from '@angular/router';
 
 import { RoomListComponent } from './room-list/room-list.component';
 import { RoomDetailComponent } from './room-detail/room-detail.component';
@@ -9,15 +9,24 @@ import {NewRoomComponent} from './new-room/new-room.component';
 const routes: Routes = [
   { path: '', redirectTo: 'room-list', pathMatch: 'full' },
   { path: 'room-list', component: RoomListComponent },
-  { path: 'room-list/:id', component: RoomDetailComponent },
+  { path: 'room-list/:id', component: RoomDetailComponent, canActivate : ['singleRoomGuard']},
   { path: 'about', loadChildren: 'app/about/about.module#LazyModule' },
   { path: 'new-room', component: NewRoomComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
+export const routingProviders = [
+  {provide: 'singleRoomGuard', useValue: singleRoomGuard}
+]
+
+export function singleRoomGuard(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+  return true;
+}
+
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [ routingProviders ]
 })
 
 export class AppRoutingModule {}
