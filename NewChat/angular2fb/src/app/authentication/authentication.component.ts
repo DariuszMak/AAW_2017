9bc8a8c4b166e113632ed0fa80685bcc11e0cd0c
DariuszMak <darekmakarewicz1994@gmail.com>
2017-05-21 14:45:20 +0200

    Logout

--- NewChat/angular2fb/src/app/authentication/authentication.component.ts
+++ NewChat/angular2fb/src/app/authentication/authentication.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthenticationService} from './authentication.service';
+import {getBootstrapListener} from "@angular/router/src/router_module";
 @Component({
   moduleId: module.id,
   selector: 'my-authentication',
@@ -8,7 +9,7 @@ import {AuthenticationService} from './authentication.service';
   // styleUrls:  ['./room-detail.component.css']
 })
 
-export class AuthenticationComponent implements OnInit{
+export class AuthenticationComponent implements OnInit, OnDestroy{
   model: any = {};
   loading = false;
   returnUrl: string;
@@ -25,15 +26,21 @@ export class AuthenticationComponent implements OnInit{
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
+  ngOnDestroy(){
+      console.log('On destroy - login');
+  }
+
   login()
   {
     this.loading = true;
     console.log('Sending username: ' + this.model.username);
+    getBootstrapListener
     this.loading = this.authenticationService.login(this.model.username);
+    this.loading = !this.loading;
 
-    if(!this.loading) this.router.navigate([this.returnUrl]);
+    if (!this.loading)
+    {
+      this.router.navigate([this.returnUrl]);
+    }
   }
-
-
-
 }
